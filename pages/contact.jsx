968ae13ff4ac15f4c7b1bf575db6b491601ede5e
Amirali <57327052+itsamirali@users.noreply.github.com>
2023-02-16 00:24:54 +0330

    connected contactus with api

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-const contact = () => {
+const ContactPage = () => {
+  const [subject, setSubject] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const contactUsHandler = () => {
+    setLoading(true);
+    let data = {};
+    if (name && email && subject) {
+      if (name) data.name = name;
+      if (email) data.email = email;
+      if (subject) data.subject = subject;
+      if (message) data.message = message;
+      axios
+        .post("http://www.weboverflow.ir/contacts/create/", data)
+        .then((res) => {
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+        });
+    }
+  };
   return (
     <div className="container">
       <h1>We Can’t Wait to Meet You!</h1>
@@ -13,20 +39,45 @@ const contact = () => {
         <input
           className="w-100 d-flex form-control me-md-4"
           placeholder="full name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
-        <input className="w-100 d-flex form-control" placeholder="email" />
+        <input
+          className="w-100 d-flex form-control"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="w-100 d-flex align-items-center justify-content-center mt-4">
-        <input className="w-100 d-flex form-control" placeholder="subject" />
+        <input
+          className="w-100 d-flex form-control"
+          placeholder="subject"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+        />
       </div>
       <div className="w-100 d-flex align-items-center justify-content-center mt-4">
-        <textarea className="w-100 d-flex form-control" placeholder="message" />
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="w-100 d-flex form-control"
+          placeholder="message"
+        />
       </div>
       <div className="w-100 d-flex align-items-center justify-content-start mt-3">
-        <button className="btn btn-success">Send</button>
+        {!loading ? (
+          <button onClick={contactUsHandler} className="btn btn-success">
+            Send
+          </button>
+        ) : (
+          <div class="spinner-grow text-dark" role="status">
+            <span class="sr-only"></span>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export default contact;
+export default ContactPage;
