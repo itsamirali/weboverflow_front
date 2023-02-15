bb6455a7d8c7e3c8b9df1528bd5cc0686d2faf5d
Amirali <57327052+itsamirali@users.noreply.github.com>
2023-02-15 22:27:40 +0330

    added portfolio page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,123 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../Images/WebOverflow_Logo.png";
+import cx from "classnames";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div
+        className={cx(styles.heroSection2, "d-flex", "flex-column", "w-100")}
+      >
+        <div
+          className={cx(
+            styles.summeryContactUsContainer,
+            "py-3",
+            "border-bottom",
+            "bg-white"
+          )}
+        >
+          <div className="container d-flex w-100 align-items-center justify-content-between">
+            <div className="d-flex align-items-center justify-content-center">
+              <span className={cx(styles.contactUsInfo)}>
+                Call: 281-782-9819
+              </span>
+              <span className={cx(styles.contactUsInfo, "mx-2")}>|</span>
+              <span className={cx(styles.contactUsInfo)}>
+                hello@weboverflow.com
+              </span>
+            </div>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="#2773BE"
+                class="bi bi-facebook"
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="w-100 bg-white">
+          <nav
+            className={cx(
+              styles.navbar,
+              "container",
+              "d-flex",
+              "flex-column",
+              "flex-xl-row",
+              "align-items-center",
+              "justify-content-xl-between",
+              "justify-content-center",
+              "py-4",
+              "position-sticky"
+            )}
+          >
+            <Image
+              src={Logo}
+              unoptimized
+              height={71}
+              alt="weboverflow"
+              className="d-flex mb-xl-0 mb-4"
+            />
+            <ul
+              className={cx(
+                styles.navbarList,
+                "d-flex align-items-center justify-content-end mb-0 p-0"
+              )}
+            >
+              <li className={cx(styles.navbarLink, "ms-xl-5", "ms-0")}>
+                <Link href="/">Home</Link>
+              </li>
+              <li className={cx(styles.navbarLink, "ms-xl-5", "ms-3")}>
+                <Link href="/portfolio">Portfolio</Link>
+              </li>
+              <li className={cx(styles.navbarLink, "ms-xl-5", "ms-3")}>
+                <Link href="/contact">Contact</Link>
+              </li>
+              <li className={cx(styles.navbarLink, "ms-xl-5", "ms-3")}>
+                <Link href="/worktogether">Let’s Work Together</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <Component {...pageProps} />
+      <footer className="w-100">
+        <div className="container d-flex align-items-center justify-content-between">
+          <div className="container d-flex w-100 align-items-center justify-content-between">
+            <div className="d-flex align-items-center justify-content-center py-4">
+              <span className={cx(styles.contactUsInfo)}>
+                Copyright 2016 WebOverflow.ir
+              </span>
+              <span className={cx(styles.contactUsInfo, "mx-2")}>|</span>
+              <span className={cx(styles.contactUsInfo)}>
+                All Rights Reserved
+              </span>
+            </div>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="#2773BE"
+                class="bi bi-facebook"
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/portfolio.jsx
@@ -0,0 +1,177 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import port1 from "../Images/port1.jpeg";
+import port2 from "../Images/port2.jpeg";
+import port3 from "../Images/port3.jpeg";
+import port4 from "../Images/port4.jpeg";
+import port5 from "../Images/port5.jpeg";
+import port6 from "../Images/port6.jpeg";
+
+import styles from "../styles/Portfolio.module.css";
+import cx from "classnames";
+
+const PortfolioPage = () => {
+  return (
+    <div className="container">
+      <div className="navbarSpacer d-flex"></div>
+      <h1>Awesome clients. Many industries.</h1>
+      <p className="my-4">Here are a few of our recent clients.</p>
+      <p className="my-4">
+        Interested in seeing what we can do for your business?{" "}
+        <Link href="/worktogether">Let’s work together</Link>! If you need help
+        immediately, you can call or text us at 281-782-9819 so we can help you
+        right away.
+      </p>
+      <div className="d-flex align-items-start justify-content-center mt-5 pt-5">
+        <div
+          className={cx(
+            styles.portInfo,
+            "d-flex",
+            "flex-column",
+            "align-items-start",
+            "justify-content-start",
+            "px-4"
+          )}
+        >
+          <h2>About Our Client</h2>
+          <p>
+            Freshful is an online only discounter fresh produce. Helping their
+            local market eat healthier and save by passing along internet-only
+            savings to their customers!
+          </p>
+          <h2>What We Did</h2>
+          <ol>
+            <li>Responsive Web Development</li>
+            <li>Online Shopping Solution</li>
+          </ol>
+        </div>
+        <Image src={port1} height={300} alt="About Our Client" />
+      </div>
+      <div className="d-flex align-items-start justify-content-center mt-5 pt-5">
+        <Image src={port2} height={300} alt="About Our Client" />
+        <div
+          className={cx(
+            styles.portInfo,
+            "d-flex",
+            "flex-column",
+            "align-items-start",
+            "justify-content-start",
+            "px-4"
+          )}
+        >
+          <h2>About Our Client</h2>
+          <p>
+            The Wireless Repair EXPO is a specialty pavilion held onsite during
+            CTIA Super Mobility 2016 in Las Vegas. savings to their customers!
+          </p>
+          <h2>What We Did</h2>
+          <ol>
+            <li>Responsive Web Development</li>
+          </ol>
+        </div>
+      </div>
+      <div className="d-flex align-items-start justify-content-center mt-5 pt-5">
+        <div
+          className={cx(
+            styles.portInfo,
+            "d-flex",
+            "flex-column",
+            "align-items-start",
+            "justify-content-start",
+            "px-4"
+          )}
+        >
+          <h2>About Our Client</h2>
+          <p>
+            Maison Maison reflects the style and taste of owner and Principal
+            Interior Designer Suzanne Duin, ASID and specializes in French
+            antiques and home accessories.
+          </p>
+          <h2>What We Did</h2>
+          <ol>
+            <li>Responsive Web Development</li>
+            <li>WordPress Online Shop</li>
+          </ol>
+        </div>
+        <Image src={port3} height={300} alt="About Our Client" />
+      </div>
+      <div className="d-flex align-items-start justify-content-center mt-5 pt-5">
+        <Image src={port4} height={300} alt="About Our Client" />
+        <div
+          className={cx(
+            styles.portInfo,
+            "d-flex",
+            "flex-column",
+            "align-items-start",
+            "justify-content-start",
+            "px-4"
+          )}
+        >
+          <h2>About Our Client</h2>
+          <p>
+            WetSeat is an easy on & off strapless seat cover that protects your
+            car’s interior. No matter how wet, dirty or messy your active
+            lifestyle is.
+          </p>
+          <h2>What We Did</h2>
+          <ol>
+            <li>Responsive Web Development</li>
+            <li>Shopify Shop Setup</li>
+          </ol>
+        </div>
+      </div>
+      <div className="d-flex align-items-start justify-content-center mt-5 pt-5">
+        <div
+          className={cx(
+            styles.portInfo,
+            "d-flex",
+            "flex-column",
+            "align-items-start",
+            "justify-content-start",
+            "px-4"
+          )}
+        >
+          <h2>About Our Client</h2>
+          <p>
+            Hurst Lending & Insurance is a Texas based mortgage and insurance
+            provider with special expertise in niche loans and meeting unique
+            lending needs.
+          </p>
+          <h2>What We Did</h2>
+          <ol>
+            <li>Maintenance & Upgrades</li>
+          </ol>
+        </div>
+        <Image src={port5} height={300} alt="About Our Client" />
+      </div>
+      <div className="d-flex align-items-start justify-content-center mt-5 pt-5">
+        <Image src={port6} height={300} alt="About Our Client" />
+        <div
+          className={cx(
+            styles.portInfo,
+            "d-flex",
+            "flex-column",
+            "align-items-start",
+            "justify-content-start",
+            "px-4"
+          )}
+        >
+          <h2>About Our Client</h2>
+          <p>
+            Erik Liljenwall Fund aims to improve the lives of next generation
+            leaders by supporting academics and educational achievements through
+            scholarships, encouraging entrepreneurial ideas through grant
+            programs, and supplying resources to human service organizations.
+          </p>
+          <h2>What We Did</h2>
+          <ol>
+            <li>Responsive Web Development</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioPage;
