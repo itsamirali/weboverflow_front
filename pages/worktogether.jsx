488cc0bbc6110373c44accb223fd03b7c114cb57
Amirali <57327052+itsamirali@users.noreply.github.com>
2023-02-16 00:41:37 +0330

    connected worktogether with api

--- pages/worktogether.jsx
+++ pages/worktogether.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-const worktogether = () => {
+const Worktogether = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [websiteUrl, setWebsiteUrl] = useState("");
+  const [hearedAboutUs, setHearedAboutUs] = useState("");
+  const [resume, setResume] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const contactUsHandler = () => {
+    setLoading(true);
+    let data = {};
+    if (name && email && subject) {
+      if (name) data.name = name;
+      if (email) data.email = email;
+      if (companyName) data.company_name = companyName;
+      if (phone) data.phone = phone;
+      if (websiteUrl) data.website_url = websiteUrl;
+      if (hearedAboutUs) data.heared_about_us = hearedAboutUs;
+      if (resume) data.resume = resume;
+      if (description) data.description = description;
+      axios
+        .post("http://www.weboverflow.ir/contacts/create/", data)
+        .then((res) => {
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+        });
+    }
+  };
   return (
     <div className="container">
       <h1>We would love to hear from you.</h1>
@@ -14,24 +47,75 @@ const worktogether = () => {
         lead to a more productive initial phone call and or meeting.
       </p>
       <strong>Thanks and talk to you soon.</strong>
-      <div className="w-100 d-flex align-items-center justify-content-center mt-4">
+      <div className="w-100 d-flex flex-column flex-md-row align-items-center justify-content-center mt-4">
         <input
-          className="w-100 d-flex form-control me-md-4"
+          className="w-100 d-flex form-control me-md-4 mb-4 mb-md-0"
           placeholder="full name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
-        <input className="w-100 d-flex form-control" placeholder="email" />
+        <input
+          className="w-100 d-flex form-control"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
-      <div className="w-100 d-flex align-items-center justify-content-center mt-4">
-        <input className="w-100 d-flex form-control" placeholder="subject" />
+      <div className="w-100 d-flex flex-column flex-md-row align-items-center justify-content-center mt-4">
+        <input
+          className="w-100 d-flex form-control me-md-4 mb-4 mb-md-0"
+          placeholder="company name"
+          value={companyName}
+          onChange={(e) => setCompanyName(e.target.value)}
+        />
+        <input
+          className="w-100 d-flex form-control"
+          placeholder="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </div>
-      <div className="w-100 d-flex align-items-center justify-content-center mt-4">
-        <textarea className="w-100 d-flex form-control" placeholder="message" />
+      <div className="w-100 d-flex flex-column flex-md-row align-items-center justify-content-center mt-4">
+        <input
+          className="w-100 d-flex form-control me-md-4 mb-4 mb-md-0"
+          placeholder="website Url"
+          value={websiteUrl}
+          onChange={(e) => setWebsiteUrl(e.target.value)}
+        />
+        <input
+          className="w-100 d-flex form-control"
+          placeholder="heared About Us"
+          value={hearedAboutUs}
+          onChange={(e) => setHearedAboutUs(e.target.value)}
+        />
+      </div>
+      <div className="w-100 d-flex flex-column flex-md-row align-items-center justify-content-center mt-4">
+        <input
+          className="w-100 d-flex form-control me-md-4 mb-4 mb-md-0"
+          placeholder="resume url link"
+          value={resume}
+          onChange={(e) => setResume(e.target.value)}
+        />
+        <input
+          className="w-100 d-flex form-control"
+          placeholder="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
       </div>
       <div className="w-100 d-flex align-items-center justify-content-start mt-3">
-        <button className="btn btn-success">Send</button>
+        {!loading ? (
+          <button onClick={contactUsHandler} className="btn btn-success">
+            Send
+          </button>
+        ) : (
+          <div class="spinner-grow text-dark" role="status">
+            <span class="sr-only"></span>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export default worktogether;
+export default Worktogether;
